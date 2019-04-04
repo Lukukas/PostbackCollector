c7f368ec20448e2197bca693874e986367e9abd1
Luke Wiedeman <luke.wiedeman@scorm.com>
2019-04-04 13:48:47 -0500

    set config vars

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const buckets = require('./router/buckets.js');
 const basicAuth = require('express-basic-auth');
-const config = require('./config.json');
 const app = express();
+let config;
+
+if (process.env.user && process.env.password) {
+    config.user = process.env.user;
+    config.password = process.env.password;
+} else {
+    config = require('./config.json');
+}
 
 app.use(basicAuth({
     users: { [config.user] : config.password }
