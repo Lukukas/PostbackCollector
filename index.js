a2be9524124d9211d6a45126f7686076945b4c09
Luke Wiedeman <luke.wiedeman@scorm.com>
2019-04-04 13:37:54 -0500

    add basic auth

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const buckets = require('./router/buckets.js');
+const basicAuth = require('express-basic-auth');
+const config = require('./config.json');
 const app = express();
+
+app.use(basicAuth({
+    users: { [config.user] : config.password }
+}));
+
 app.use('/buckets', buckets);
+
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('OK!!!');
-});
 app.listen(port);
