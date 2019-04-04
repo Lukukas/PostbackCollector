77c115b5da3b46049324995217ec23b2d8d00986
Luke Wiedeman <luke.wiedeman@scorm.com>
2019-04-04 13:07:35 -0500

    some additional touch ups

--- router/buckets.js
+++ router/buckets.js
@@ -18,51 +18,110 @@ const store = keyVStore.create(folder, (err) => {
 
 router.post('/', async (req, res) => {
     const bucketId = Toolbox.createRandomString();
-    await store.set(bucketId, []);
-    res.statusCode = 201;
-    res.json({
-        "bucketId": bucketId
-    });
+    try {
+        await store.set(bucketId, []);
+        res.statusCode = 201;
+        res.json({
+            "bucketId": bucketId
+        });
+    } catch (e) {
+        res.statusCode = 500;
+        res.json({ "error": "something went wrong :("})
+        console.log(e);
+    }
 });
 
 router.get('/', async (req, res) => {
-    const stream = store.getAll();
-    let arr = [];
-    stream.on('readable', () => {
-        const entry = stream.read();
-        if(entry !== null) {
-            arr.push(entry.key);
-        }
-    });
-    stream.on('end', () => {
-        res.json({ keys: arr });
-    });
+    try {
+        const stream = store.getAll();
+        let arr = [];
+        stream.on('readable', () => {
+            const entry = stream.read();
+            if(entry !== null) {
+                arr.push(entry.key);
+            }
+        });
+        stream.on('end', () => {
+            res.json({ keys: arr });
+        });
+    } catch (e) {
+        res.statusCode = 500;
+        res.json({ "error": "something went wrong :("})
+        console.log(e);
+    }
 });
 
 router.get('/:bucketId', async (req, res) => {
-    const data = await store.get(req.params.bucketId);
-    res.json({postbacks: data});
+    try {
+        const data = await store.get(req.params.bucketId);
+        if (data) {
+            res.json({postbacks: data});
+        } else {
+            res.statusCode = 404;
+            res.send();
+        }
+        
+    } catch (e) {
+        res.statusCode = 500;
+        res.json({ "error": "something went wrong :("})
+        console.log(e);
+    }
 });
 
 router.get('/:bucketId/latest', async (req, res) => {
-    const data = await store.get(req.params.bucketId);
-    res.json({postback: data[data.length-1]});
+    try {
+        const data = await store.get(req.params.bucketId);
+        if (data) {
+            res.json({postback: data[data.length-1]});
+        } else {
+            res.statusCode = 404;
+            res.send();
+        }
+        
+    } catch (e) {
+        res.statusCode = 500;
+        res.json({ "error": "something went wrong :("})
+        console.log(e);
+    }
 });
 
 router.delete('/:bucketId', async (req, res) => {
-    await store.delete(req.params.bucketId);
-    res.statusCode = 204;
-    res.send();
+    try {
+        const check = await store.get(req.params.bucketId);
+        if (check) {
+            await store.delete(req.params.bucketId);
+            res.statusCode = 204;
+            res.send();
+        } else {
+            res.statusCode = 404;
+            res.send();
+        }
+    } catch (e) {
+        res.statusCode = 500;
+        res.json({ "error": "something went wrong :("})
+        console.log(e);
+    }
 });
 
 router.post('/:bucketId', async (req, res) => {
-    const dataArr = await store.get(req.params.bucketId);
-    const decodedBody = decodeURIComponent(req.body).replace(/\+/g, ' ');
-    const bodyObject = convert.xml2js(decodedBody, { compact: true, textKey: 'value', attributesKey: 'value', commentKey: 'value' });
-    dataArr.push(bodyObject);
-    await store.set(req.params.bucketId, dataArr);
-    res.statusCode = 201;
-    res.send();
+    try {
+        const dataArr = await store.get(req.params.bucketId);
+        if (dataArr) {
+            const decodedBody = decodeURIComponent(req.body).replace(/\+/g, ' ');
+            const bodyObject = convert.xml2js(decodedBody, { compact: true, textKey: 'value', attributesKey: 'value', commentKey: 'value' });
+            dataArr.push(bodyObject);
+            await store.set(req.params.bucketId, dataArr);
+            res.statusCode = 201;
+            res.send();
+        } else {
+            res.statusCode = 404;
+            res.send();
+        }
+    } catch (e) {
+        res.statusCode = 500;
+        res.json({ "error": "something went wrong :("})
+        console.log(e);
+    }
 });
 
 module.exports = router;
