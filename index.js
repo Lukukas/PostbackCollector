9c73ac0778de4881472813a3144161cd90a6db91
Luke Wiedeman <luke.wiedeman@scorm.com>
2020-04-27 11:20:14 -0500

    add zip file downloads

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const buckets = require('./router/buckets.js');
 const unsecureBuckets = require('./router/unsecureBuckets.js');
+const zipFiles = require('./router/zipFiles.js');
 const basicAuth = require('express-basic-auth');
 const app = express();
 
 app.use('/buckets', buckets);
 app.use('/unsecureBuckets', unsecureBuckets);
+app.use('/zipFiles', zipFiles);
 
 const port = process.env.PORT || 3000;
 
---
+++ router/zipFiles.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+const zips = path.join(__dirname, '..', 'zips');
+const Toolbox = require('../toolbox');
+
+router.get('/aicc', (req, res) => {
+    const zip = path.join(zips, 'AICC.zip');
+    res.download(zip);
+});
+
+router.get('/cmi5', (req, res) => {
+    const zip = path.join(zips, 'cmi5.zip');
+    res.download(zip);
+});
+
+router.get('/scorm12', (req, res) => {
+    const zip = path.join(zips, 'SCORM12.zip');
+    res.download(zip);
+});
+
+router.get('/scorm20043rd', (req, res) => {
+    const zip = path.join(zips, 'SCORM20043rdEdition.zip');
+    res.download(zip);
+});
+
+router.get('/xapi', (req, res) => {
+    const zip = path.join(zips, 'xAPI.zip');
+    res.download(zip);
+});
+
+router.get('/notazip', (req, res) => {
+    const zip = path.join(zips, 'notAZip.csv');
+    res.download(zip);
+});
+
+router.get('/withexe', (req, res) => {
+    const zip = path.join(zips, 'SCORM12WITHEXE.zip');
+    res.download(zip);
+});
+
+router.get('/witheicar', (req, res) => {
+    const zip = path.join(zips, 'SCORM12WITHEICAR.zip');
+    res.download(zip);
+});
+
+router.get('/withhiddenexe', (req, res) => {
+    const zip = path.join(zips, 'SCORM12WITHHIDDENEXE.zip');
+    res.download(zip);
+});
+
+router.get('/wothhiddeneicar', (req, res) => {
+    const zip = path.join(zips, 'SCORM12WITHHIDDENEICAR.zip');
+    res.download(zip);
+});
+
+module.exports = router;
